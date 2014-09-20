cc09407ebd740d418190ee1ed1f9c2063f66aaf4
Javier Carrillo <stringparser@gmail.com>
2014-09-21 00:24:09 +0200

    .gitignore

---
+++ test/test.batch-these.js
@@ -0,0 +1,19 @@
+'use strict';
+
+describe('batch-these', function(){
+
+  var fs = require('fs');
+  var batchThese = require('../.');
+  var testFiles = fs.readdirSync('./test');
+
+  testFiles.splice( testFiles.indexOf(__filename), 1);
+
+  testFiles.forEach(function(testFile){
+    var suite = testFile.match(/\w+/)[0];
+    describe(suite, function(){
+      require(testFile)(batchThese);
+    });
+
+  });
+
+});
