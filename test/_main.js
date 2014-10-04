118cdd434e188526ee388251f36303aa32bac5f2
Javier Carrillo <stringparser@gmail.com>
2014-10-04 20:39:09 +0200

    unnecesary lame names for the test files

--- test/_main.js
+++ test/_main.js
@@ -10,7 +10,7 @@ describe('batch-these', function(){
   testFiles.splice( testFiles.indexOf(path.basename(__filename)), 1);
 
   testFiles.forEach(function(testFile){
-    var suite = testFile.match(/\w+/)[0];
+    var suite = path.basename(testFile, path.extname(testFile));
     describe(suite, function(){
       require('./'+testFile)(batchThese);
     });
